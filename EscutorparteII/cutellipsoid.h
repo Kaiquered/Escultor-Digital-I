7af189e82ea6a54a2f1fc353ef0d1cc11079fcf9
Kaique Gomes Machado <kaiquered@hotmail.com>
2019-10-18 01:38:11 -0300

    Update cutellipsoid.h

--- EscutorparteII/cutellipsoid.h
+++ EscutorparteII/cutellipsoid.h
@@ -3,7 +3,7 @@
 #include <figurageometrica.h>
 
 
-/* @brief Classe herdeira de FiguraGeometrica,
+/** @brief Classe herdeira de FiguraGeometrica,
 * responsável por adicionar elipsoides em nossa escultura.
 * note que é bem parecido (e pode ser igual caso rx_ = ry_ = rz_) 
 * à subclasse responsável por adicionar esferas
@@ -15,10 +15,10 @@ protected:
     int xcenter,ycenter,zcenter,rx,ry,rz;
 public:
     Cutellipsoid(int xcenter_, int ycenter_, int zcenter_, int rx_, int ry_, int rz_);
-    /* @brief construtor da subclasse
+    /** @brief construtor da subclasse
     */
     ~Cutellipsoid();
-    /* @brief virtualização do método através do draw
+    /** @brief virtualização do método através do draw
     */
     void draw(Sculptor &t);
 };
