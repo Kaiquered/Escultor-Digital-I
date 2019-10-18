c235bc7ead18dd282b99bb49f17a0e7fd6b70e24
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-10-17 21:27:05 -0300

    adicionando documentação doxygen

--- EscutorparteII/sculptor.h
+++ EscutorparteII/sculptor.h
@@ -1,18 +1,28 @@
 #ifndef SCULPTOR_H
 #define SCULPTOR_H
 
+/* @brief Estrutura que define como serão os pontos da escultura.
+* r, g e b são variáveis de cor;
+* a determina transparência;
+* isOn determina se o bloco está ou não na escultura.
+*/
+
 struct Voxel {
   float r,g,b;
   float a;
   bool isOn;
 };
 
+/* @brief Classe que reune as informações fornecidas pela estrutura Voxel
+* e aplica os metódos para, de fato, montar a escultura.
+*/
+
 class Sculptor {
 protected:
   Voxel ***v;
   int nx,ny,nz;
   float r,g,b,a;
-
+  
 public:
   Sculptor(int _nx, int _ny, int _nz);
   ~Sculptor();
