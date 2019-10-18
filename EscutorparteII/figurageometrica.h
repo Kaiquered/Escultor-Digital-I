83cf142baf14583a2e1395b8a6db61b1900cd5bd
Kaique Gomes Machado <kaiquered@hotmail.com>
2019-10-18 01:42:05 -0300

    Update figurageometrica.h

--- EscutorparteII/figurageometrica.h
+++ EscutorparteII/figurageometrica.h
@@ -2,18 +2,13 @@
 #define FIGURAGEOMETRICA_H
 #include <sculptor.h>
 
-/* @brief Classe abstrata FiguraGeometrica, para tornar o metódo
-* draw virtusl, e posteriormente o utilizar em suas classes herdeiras.
+/** @brief Classe abstrata FiguraGeometrica, para tornar o metódo
+* draw virtual, e em seguida ser implementado pelas classes herdeiras.
 */
 
 class FiguraGeometrica{
 public:
   FiguraGeometrica();
-
-/** @brief Método virtual draw que embora não seja definido
-* na classe FiguraGeometrica, executa o mesmo método virtual das
-* subclasses (as herdeiras).
-*/
   
   virtual void draw(Sculptor &t)=0;
 
