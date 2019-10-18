35c9fc1c699c9b49454d0382d6717091742b9dd9
Kaique Gomes Machado <kaiquered@hotmail.com>
2019-10-18 01:40:40 -0300

    Update cutvoxel.h

--- EscutorparteII/cutvoxel.h
+++ EscutorparteII/cutvoxel.h
@@ -2,7 +2,7 @@
 #define CUTVOXEL_H
 #include <figurageometrica.h>
 
-/* @brief Classe herdeira de FiguraGeometrica,
+/** @brief Classe herdeira de FiguraGeometrica,
 * responsável por remover pontos em nossa escultura.
 */
 
@@ -13,11 +13,9 @@ protected:
 
 public:
     Cutvoxel(int x_, int y_, int z_);
-    /* @brief construtor da subclasse
-    */
+   
     ~Cutvoxel();
-    /* @brief virtualização do método através do draw
-    */
+    
     void draw(Sculptor &t);
 
 };
