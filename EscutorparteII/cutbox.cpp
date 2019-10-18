c39ae8dfd253c465e97c109940547d0854ba6702
Kaique Gomes Machado <kaiquered@hotmail.com>
2019-10-18 00:02:06 -0300

    correção do for

--- EscutorparteII/cutbox.cpp
+++ EscutorparteII/cutbox.cpp
@@ -19,9 +19,9 @@ Cutbox::~Cutbox()
 
 void Cutbox::Cutbox::draw(Sculptor &t)
 {
-           for (int i = x0; i<=x1; i++){
-             for (int j = y0; j<=y1; j++){
-                 for (int k = z0; k<=z1; k++){
+           for (int i = x0; i<x1; i++){
+             for (int j = y0; j<y1; j++){
+                 for (int k = z0; k<z1; k++){
                      t.cutVoxel(i,j,k);
                  }
              }
