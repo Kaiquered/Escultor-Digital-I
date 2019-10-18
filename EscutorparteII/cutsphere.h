89f6323b27e9cd08db16ab805bbf1e7bef1036a5
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-10-17 21:16:12 -0300

    adicionando documentação doxygen

--- EscutorparteII/cutsphere.h
+++ EscutorparteII/cutsphere.h
@@ -2,6 +2,10 @@
 #define CUTSPHERE_H
 #include <figurageometrica.h>
 
+/* @brief Classe herdeira de FiguraGeometrica,
+* responsável por remover esferas em nossa escultura.
+*/
+
 class Cutsphere : public FiguraGeometrica
 {
 protected:
@@ -9,7 +13,11 @@ protected:
 
 public:
     Cutsphere(int xcenter_, int ycenter_, int zcenter_, int radius_);
+    /* @brief construtor da subclasse
+    */
     ~Cutsphere();
+    /* @brief virtualização do método através do draw
+    */
     void draw(Sculptor &t);
 };
 
