bdf1b79abf89416ea610146e742503255c0cd090
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-10-17 21:08:08 -0300

    adicionando documentação doxygen

--- EscutorparteII/putvoxel.h
+++ EscutorparteII/putvoxel.h
@@ -2,6 +2,9 @@
 #define Putvoxel_H
 #include <figurageometrica.h>
 
+/* @brief Classe herdeira de FiguraGeometrica,
+* responsável por adicionar pontos em nossa escultura.
+*/
 
 class Putvoxel : public FiguraGeometrica
 {
@@ -10,7 +13,11 @@ class Putvoxel : public FiguraGeometrica
     float r, g, b, a;
     public:
     Putvoxel(int x_,int y_,int z_, float r_,float g_,float b_,float a_);
+    /* @brief construtor da subclasse
+    */
     ~Putvoxel();
+    /* @brief virtualização do método através do draw
+    */
     void draw(Sculptor &t);
 };
 
