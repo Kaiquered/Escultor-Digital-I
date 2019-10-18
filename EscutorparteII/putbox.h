260f205e5095ff4fb2792a7459c785a0fa7492ec
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-10-17 21:12:02 -0300

    adicionando documentação doxygen

--- EscutorparteII/putbox.h
+++ EscutorparteII/putbox.h
@@ -3,6 +3,10 @@
 #include <figurageometrica.h>
 
 
+/* @brief Classe herdeira de FiguraGeometrica,
+* responsável por adicionar caixas (cúbicas ou não) em nossa escultura.
+*/
+
 class Putbox : public FiguraGeometrica
 {
 protected :
@@ -10,7 +14,11 @@ protected :
     float r,g,b,a;
 public:
     Putbox(int x_0, int x_1, int y_0, int y_1, int z_0, int z_1, float r_, float g_, float b_, float a_);
+    /* @brief construtor da subclasse
+    */
     ~Putbox();
+    /* @brief virtualização do método através do draw
+    */
     void draw(Sculptor &t);
 };
 
