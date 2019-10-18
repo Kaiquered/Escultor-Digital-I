f8de31edc5e48703e77ee45af1c098540cbdae39
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-10-17 21:14:54 -0300

    adicionando documentação doxygen

--- EscutorparteII/putsphere.h
+++ EscutorparteII/putsphere.h
@@ -2,6 +2,9 @@
 #define PUTSPHERE_H
 #include <figurageometrica.h>
 
+/* @brief Classe herdeira de FiguraGeometrica,
+* responsável por adicionar esferas em nossa escultura.
+*/
 
 class Putsphere : public FiguraGeometrica
 {
@@ -12,7 +15,11 @@ protected:
 
 public:
     Putsphere(int xcenter_, int ycenter_, int zcenter_, int radius_,float r_,float g_,float b_,float a_);
+    /* @brief construtor da subclasse
+    */
     ~Putsphere();
+    /* @brief virtualização do método através do draw
+    */
     void draw(Sculptor &t);
 };
 
