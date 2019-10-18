4d25b247452429fffec9f157ff3aae9c67588748
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-10-17 21:20:37 -0300

    adicionando documentação doxygen

--- EscutorparteII/putellipsoid.h
+++ EscutorparteII/putellipsoid.h
@@ -2,6 +2,12 @@
 #define PUTELLIPSOID_H
 #include <figurageometrica.h>
 
+/* @brief Classe herdeira de FiguraGeometrica,
+* responsável por remover elipsoides em nossa escultura.
+* note que é bem parecido (e pode ser igual caso rx_ = ry_ = rz_) 
+* à subclasse responsável por remover esferas
+*/
+
 class Putellipsoid : public FiguraGeometrica
 {
 protected:
@@ -9,7 +15,11 @@ protected:
     float r,g,b,a;
 public:
     Putellipsoid(int xcenter_, int ycenter_, int zcenter_, int rx_, int ry_, int rz_,float r_,float g_,float b_,float a_);
+    /* @brief construtor da subclasse
+    */
     ~Putellipsoid();
+    /* @brief virtualização do método através do draw
+    */
     void draw(Sculptor &t);
 };
 
